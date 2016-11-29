c56ee373078fd60131ade80722ec06a248f4e000
Charles Lu <charleslu@live.com>
2016-11-28 17:11:47 -0800

    Stub auction classes

---
+++ auctions.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+class AuctionProtocol(object):
+    def __init__(self):
+        self.bids = []
+
+class VCGAuction(AuctionProtocol):
+    def __init__(self):
+        pass
+
+class GMSMAAuction(AuctionProtocol):
+    def __init__(self):
+        pass
